56c5a6a8c679fefbfc4a5de24c918dc395049a3e
IxAntho <isabar735@icloud.com>
2024-04-25 19:51:26 -0400

    feat/render movies, edit and delete items

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Float
+from sqlalchemy import Integer, String, Float, select
+from sqlalchemy.exc import IntegrityError
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, FloatField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 import os
@@ -40,6 +41,7 @@ class Movie(db.Model):
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
 
+# Create db
 with app.app_context():
     if not os.path.exists(DB_PATH):
         db.create_all()
@@ -48,9 +50,82 @@ with app.app_context():
         print("Database already exists.")
 
 
+# Edit form
+class EditForm(FlaskForm):
+    rating = FloatField('Your rating out of 10, e.g. 7.5', validators=[DataRequired()])
+    review = StringField('Your review', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+# # Testing adding a new movie
+# with app.app_context():
+#     new_movie = Movie(
+#         title="Phone Booth",
+#         year=2002,
+#         description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#         rating=7.3,
+#         ranking=10,
+#         review="My favourite character was the caller.",
+#         img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+#     )
+#     db.session.add(new_movie)
+#
+#     try:
+#         db.session.commit()
+#     except IntegrityError as e:
+#         print(f"Error: {str(e)}")
+#         db.session.rollback()
+#
+# with app.app_context():
+#     second_movie = Movie(
+#         title="Avatar The Way of Water",
+#         year=2022,
+#         description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+#         rating=7.3,
+#         ranking=9,
+#         review="I liked the water.",
+#         img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+#     )
+#     db.session.add(second_movie)
+#
+#     try:
+#         db.session.commit()
+#     except IntegrityError as e:
+#         print(f"Error: {str(e)}")
+#         db.session.rollback()
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    with app.app_context():
+        result = db.session.execute(db.select(Movie).order_by(Movie.ranking))
+        all_movies = result.scalars().all()
+    return render_template("index.html", movies=all_movies)
+
+
+@app.route("/edit/<int:movie_id>", methods=["GET", "POST"])
+def edit(movie_id):
+    movie = db.get_or_404(Movie, movie_id)
+
+    form = EditForm(obj=movie)
+    if form.validate_on_submit():
+        # Different way to update a movie entry,
+        # since we previously get hold of the movie object what's left is
+        # just change its attributes' values just like any other variable
+        movie.rating = form.rating.data
+        movie.review = form.review.data
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    return render_template("edit.html", form=form, movie=movie)
+
+
+@app.route("/delete/<int:movie_id>")
+def delete(movie_id):
+    movie_to_delete = db.get_or_404(Movie, movie_id)
+    db.session.delete(movie_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
